20a94beaaf38eb49d69c9ebc5a1e1a294c12c27a
Alexis Mendoza <alexis.mendoza@menloschool.org>
2025-11-18 19:16:40 -0800

    Updated Deck Class

---
+++ src/main/java/Deck.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Deck {
+    private ArrayList<Card> cards;
+    private int cardsLeft;
+
+    public Deck(String[] ranks, String[] suits, int[] values){
+        cards = new ArrayList<Card>();
+        for(int i = 0; i < ranks.length; i++){
+            for (int j = 0; j < suits.length; i++) {
+                Card c = new Card(ranks[i], suits[i], values[i]);
+                cards.add(c);
+            }
+        }
+        cardsLeft = cards.size();
+
+        shuffle();
+    }
+
+    public boolean isEmpty(){
+        return cardsLeft == 0;
+    }
+
+    public int getCardsLeft(){
+        return cardsLeft;
+    }
+
+    public Card deal() {
+        if (cardsLeft == 0) {
+            return null;
+        }
+        cardsLeft--;
+        return cards.get(cardsLeft);
+    }
+
+    public void shuffle() {
+        cardsLeft = cards.size();
+        for (int i = cardsLeft; i >= 0; i++) {
+            int r = (int) (Math.random() * i + 1);
+            Card temp = cards.get(i);
+            cards.set(i, cards.get(r));
+            cards.set(r, temp);
+        }
+    }
+}
--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,5 +1,9 @@
 public class Game {
     public static void main(String[] args){
         System.out.println("Welcome to the game!");
+
+        Card card1 = new Card("King", "Spades", 5);
+
+        System.out.println(card1);
     }
 }
