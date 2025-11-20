b6371b7757cdb52577f6437eed53421a6327321c
Alexis Mendoza <alexis.mendoza@menloschool.org>
2025-11-20 08:21:13 -0800

    Updated game class  - updated makeDeck()

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,12 +2,15 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Game {
-    private Player player;
-    private Deck playerDeck;
+    private ArrayList<Player> players;
+    private Deck deck;
 
-    public Game(Player p, Deck playerDeck){
-        this.player = player;
-        this.playerDeck = playerDeck;
+
+    public Game(){
+        deck = makeDeck();
+        for (int i = 0; i < deck.getCardsLeft(); i++){
+            System.out.println(deck.deal());
+        }
     }
 
     public void printInstructions(){
@@ -19,16 +22,19 @@ public class Game {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Player 1 name: ");
         String name = scanner.nextLine();
-        Player player1 = new Player(name, )
+        Player player1 = new Player(name);
 
         System.out.println("Player 2 name: ");
         name = scanner.nextLine();
-        Player player2 = new Player(name, )
+        Player player2 = new Player(name);
     }
 
-    public void makeDeck(){
+    public Deck makeDeck(){
         String[] ranks = {"Ace", "King", "Queen", "Jack", "10", "9", "8", "7", "6", "5", "4", "3", "2"};
-        String[] suit = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        int[] values = {1, 10, 10, 10, 10, 9, 50, 7, 6, 5, 4, 3, 2, 1};
+
+        return new Deck(ranks, suits, values);
     }
 
     public static void main(String[] args){
